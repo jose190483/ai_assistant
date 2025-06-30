8f2c8569976d483c54070aeaf8201551ec87929c
josevinoth <101917197+josevinoth@users.noreply.github.com>
2025-06-30 22:20:23 +0530

    views & Urls

---
+++ idp/idp_app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+from .sub_views.search_keywords import search_keywords
+from .sub_views.manage_pdfs import manage_pdfs
+from .sub_views.export_csv import export_csv
